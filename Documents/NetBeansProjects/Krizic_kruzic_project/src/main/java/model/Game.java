bb34eb98cfaaa1735a33f2ccabbc66c0ce439543
Korisnik <Korisnik@192.168.10.201>
2018-01-25 13:50:54 +0100

    Postavka igre Krizic_kruzic

--- Documents/NetBeansProjects/Krizic_kruzic_project/src/main/java/model/Game.java
+++ Documents/NetBeansProjects/Krizic_kruzic_project/src/main/java/model/Game.java
@@ -1,28 +1,58 @@
 
 package model;
 
-import com.fasterxml.jackson.annotation.JacksonInject;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.annotation.JsonSetter;
+import java.util.ArrayList;
+import java.util.List;
 
 
-
+@JsonIgnoreProperties({"computer","human"})
 public class Game {
     
     
-    private final long gameId;
+    private  long gameId;
+    private static String computer="computer";
+    private  String human;
     
     
-    
-    public Game(long gameId){
+    public Game(long gameId, String human){
+        
         this.gameId=gameId;  
+        this.human=human;
     }
-
+    
+   
+    
+    
+    public Game(){   
+    }
+    public List getGameStatus(List gameStatus){
+        return gameStatus;
+    }
+   
     @JsonProperty("gameId")
     public long getId(){
         return gameId;
     }
+    
+    public String getComputer(){
+        return computer;
+    }
+    public String getHuman(){
+        return human;
+    }
+    public void setId(long gameId){
+        this.gameId=gameId;
+    }
+    public void setHuman(String human){
+        this.human= human;
+    }
+   
+   
     
    
     
